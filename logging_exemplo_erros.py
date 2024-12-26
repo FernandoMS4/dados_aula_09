fb47609b4a7b219716687cd34c86248c7f457b72
FernandoMS4 <fernando381046@gmail.com>
2024-12-26 11:12:49 -0300

    Testes sentry

---
+++ logging_exemplo_erros.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+logger.debug("Um aviso para o dev")
+logger.info("Uma informação importante do processo")
+logger.warning("Um aviso que algo vai parar de funcionar no futuro")
+logger.error("Aconteceu um a falha")
+logger.critical("Deu muito ruim corre berg")
---
+++ sentry_exemplo.py
@@ -0,0 +1,33 @@
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://ac5057b07a0267bd86549786ea2fd362@o4508534185984000.ingest.us.sentry.io/4508534202040320",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+)
+
+def slow_function():
+    import time
+    time.sleep(0.1)
+    return "done"
+
+def fast_function():
+    import time
+    time.sleep(0.05)
+    return "done"
+
+# Manually call start_profiler and stop_profiler
+# to profile the code in between
+sentry_sdk.profiler.start_profiler()
+for i in range(0, 10):
+    slow_function()
+    fast_function()
+
+division_by_zero = 1 / 0
+#
+# Calls to stop_profiler are optional - if you don't stop the profiler, it will keep profiling
+# your application until the process exits or stop_profiler is called.
+sentry_sdk.profiler.stop_profiler()
+
+
